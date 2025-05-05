b43302f9006dd4c873a07fea66b717e55423ea69
Sylvester Nkwonta <nkwontasylvester15@gmail.com>
2025-05-05 14:55:30 +0100

    Add files via upload

---
+++ Newproject/booI.py
@@ -0,0 +1,9 @@
+# Boolean
+# This module provides a class for representing boolean values and performing logical operations on them.
+
+x = ""
+print(bool(x)) # False # empty string is false 
+y = 'hello'
+print(bool(y)) # True # non empty string is true
+z = 0 
+print(bool(z)) # False # 0 is false
