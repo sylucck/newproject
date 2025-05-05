b2bdb19162309c141b37d68668cf2af998867dfb
Sylvester Nkwonta <nkwontasylvester15@gmail.com>
2025-05-05 14:40:36 +0100

    Add files via upload

---
+++ Newproject/typeconversion.py
@@ -0,0 +1,22 @@
+x = input( "Enter a number: ") # input is a string
+print(type(x)) # <class 'str'> # input is a string
+#print(x) # 5 # input is a string
+# print(x + 5) # 55 # input is a string
+# we can convert the string to an interger using the int() function
+# int() function converts a string to an interger
+# float() function converts a string to a float
+# complex() function converts a string to a complex number
+# bool() function converts a string to a boolean value
+# str() funtion converts a string to a string
+# list() function converts a string to a list
+# tuple() function converts a string to a tuple
+# set() function converts a string to a set
+# dict() function converts a string to a dictionary
+# frozenset() function coonverts a string to a frozenset
+# bytes() function converts a string to a bytes objesct
+print(int(x) + 5) # 10 # input is a string 
+y = int(x) + 50
+print(f'{x}, {y}')
+print(type(y)) # <class 'int'> # y is an interger
+
+
