54af285feb288445e35337e7bb63438f7463e328
Sylvester Nkwonta <nkwontasylvester15@gmail.com>
2025-05-12 03:40:23 +0100

    Add files via upload

---
+++ Newproject/functions.py
@@ -0,0 +1,14 @@
+# Funtions
+def greet():
+    print("My name is Sly")
+    print("I am Sly")
+
+greet()
+
+def greet(first_name, last_name):
+    print(f"Hi, {first_name} {last_name}")
+    print("Welcome on board")
+
+    
+
+greet("Sly", "Johnson")
